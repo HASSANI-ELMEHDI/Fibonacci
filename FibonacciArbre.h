107dceec79244c513025d855e0a8b5edd91ba253
HASSANI-ELMEHDI <105174552+HASSANI-ELMEHDI@users.noreply.github.com>
2023-01-13 22:15:21 +0100

    Update FibonacciArbre.h

--- FibonacciArbre.h
+++ FibonacciArbre.h
@@ -1,2 +1,18 @@
 
 
+
+#ifndef ARBRE_FIBONACCITREE_H
+#define ARBRE_FIBONACCITREE_H
+#include "Fibonacci_Node.cpp"
+#include "Arbre.cpp"
+class FibonacciTree {
+public:
+    Arbre<Fibonacci_Node> *arbre{};
+    FibonacciTree();
+    long double add_fibo_node(Fibonacci_Node) const;
+    long double fibonacci_of(long double );
+    long double calc_fibonacci(long double );
+    };
+
+
+#endif //ARBRE_FIBONACCITREE_H
